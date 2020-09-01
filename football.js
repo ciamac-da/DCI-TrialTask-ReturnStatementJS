c0994ea45c686f1c873d5bfc7dd0de4da4ca5b69
ciamac-da <ciamac.davoudi@gmail.com>
2020-09-01 15:05:10 +0200

    verified

--- football.js
+++ football.js
@@ -1,14 +1,14 @@
 // The first solution
 
-function fottballPoints(wins, draws, losses){
+function footballPoints(wins, draws, losses){
     return wins*3 + draws 
 }
-console.log("the first solution ", fottballPoints(3,3,90))
+console.log("the first solution ", footballPoints(3,3,90))
 
 
 // The second one 
-fottballPoints2 = (w,d,l) =>(3*w)+d
-console.log("the second solution ", fottballPoints2(2,3,3))
+footballPoints2 = (w,d,l) =>(3*w)+d
+console.log("the second solution ", footballPoints2(2,3,3))
 
 
 // The third  solution
@@ -17,7 +17,4 @@ console.log("the second solution ", fottballPoints2(2,3,3))
  }
  var points = x(3,3,3)
 console.log("the third solution ", points)
-
-
-
 
